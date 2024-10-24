6257c1527fb9e0b15fddf2c69c036124432a5e6f
Tegan Jennings <teganjennings1@hotmail.com>
2024-10-24 16:15:52 +0100

    Adding Create Project functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components.jsx/NewProject";
+import NoProjectSelected from "./components.jsx/NoProjectSelected";
 import SideBar from "./components.jsx/Sidebar";
 
 function App() {
+	const [projectsState, setProjectsState] = useState({
+		selectedProjectId: undefined,
+		proejcts: [],
+	});
+
+	function handleStartAddProject() {
+		setProjectsState((prevState) => {
+			return {
+				...prevState,
+				selectedProjectId: null,
+			};
+		});
+	}
+
+	let content;
+
+	if (projectsState.selectedProjectId === null) {
+		content = <NewProject />;
+	} else if (projectsState.selectedProjectId === undefined) {
+		content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+	}
+
 	return (
 		<main className="h-screen my-8 flex gap-8">
-			<SideBar />
-			<NewProject />
+			<SideBar onStartAddProject={handleStartAddProject} />
+			{content}
 		</main>
 	);
 }
