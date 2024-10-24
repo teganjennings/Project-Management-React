21e6ef731976d6e44a3f3c62318637c1131ccd48
Tegan Jennings <teganjennings1@hotmail.com>
2024-10-24 18:37:14 +0100

    Adding delete task functionality

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,14 @@ function App() {
 		});
 	}
 
-	function handleDeleteTask() {}
+	function handleDeleteTask(id) {
+		setProjectsState((prevState) => {
+			return {
+				...prevState,
+				tasks: prevState.tasks.filter((task) => task.id !== id),
+			};
+		});
+	}
 
 	function handleSelectProject(id) {
 		setProjectsState((prevState) => {
